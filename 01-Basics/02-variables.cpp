0f00de1c0558594acc03eb1b3746c6eed1c1c784
Satyam Kumar <satyam31sk@gmail.com>
2026-07-22 19:07:22 +0530

    create 02-variables.cpp

---
+++ 01-Basics/02-variables.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age = 25; // Variable declaration and initialization
+    double height = 5.9; // Variable declaration and initialization
+    char grade = 'A'; // Variable declaration and initialization
+    bool isStudent = true; // Variable declaration and initialization
+
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Is Student: " << isStudent << endl;
+
+    return 0;
+}
