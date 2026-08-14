f2ecb5bdbf38d26bfd3fdb2d0149f11084f43b53
Satyam Kumar <satyam31sk@gmail.com>
2026-08-14 09:54:05 +0530

    add 04-linked-list.cpp

---
+++ 01-Basics/04-linked-list.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+int main() {
+    Node* head = new Node{10, nullptr};
+    head->next = new Node{20, nullptr};
+    head->next->next = new Node{30, nullptr};
+
+    Node* temp = head;
+
+    while (temp != nullptr) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    return 0;
+}
