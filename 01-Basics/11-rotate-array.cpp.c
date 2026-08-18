d52099e189395a246bda7cf5c84847d8f111e02e
Satyam Kumar <satyam31sk@gmail.com>
2026-08-18 11:22:39 +0530

    add 11-rotate-array.cpp.c

---
+++ 01-Basics/11-rotate-array.cpp.c
@@ -0,0 +1,15 @@
+// Given an array rotate to the right by k positions
+
+#include <iostream>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+
+    int a[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
