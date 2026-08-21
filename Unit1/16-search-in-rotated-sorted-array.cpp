7f23ce124e512f75221050003e7ef3085d3414a3
Satyam Kumar <satyam31sk@gmail.com>
2026-08-21 14:02:27 +0530

    add 16-search-in-rotated-sorted-array.cpp

---
+++ Unit1/16-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,3 @@
+// A sorted array has been rotated at an unknown pivot point. 
+// Given the rotated array and a target key, find the index of the target element 
+// using an algorithm with O(log n) time complexity.
