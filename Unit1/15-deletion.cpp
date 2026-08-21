e3f73129b5e09c157a7f521da8dbba724a1aa3bc
Satyam Kumar <satyam31sk@gmail.com>
2026-08-21 13:38:44 +0530

    edit 15-deletion.cpp

--- Unit1/15-deletion.cpp
+++ Unit1/15-deletion.cpp
@@ -1,3 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+int main() {
+    int arr[] = {3, 12, 21, 45, 67};
+    int n = 5;
+    int pos = 2;
+
+    for (int i = pos; i < n - 1; i++)
+        arr[i] = arr[i + 1];
+
+    n--;
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
